67b0071cde5eb72d99f866302097cf74def62c23
Jaydenhebert22 <56656273+Jaydenhebert22@users.noreply.github.com>
2019-10-22 16:01:15 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,52 @@
 
 <!-- Retrieve all posts from database  -->
 <?php $posts = getPublishedPosts(); ?>
-<?php require_once( ROOT_PATH . '/includes/head_section.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
+	<!-- Styling for public area -->
+	<link rel="stylesheet" href="static/css/public_styling.css">
+	<meta charset="UTF-8">
 	<title>WTF Weekly | Home </title>
 </head>
 <body>
 	<!-- container - wraps whole page -->
 	<div class="container">
 		<!-- navbar -->
-        <?php include( ROOT_PATH . '/includes/navbar.php') ?>
+        <div class="navbar">
+	<div class="logo_div">
+		<a href="index.php"><h1>WTF Weekly</h1></a>
+	</div>
+	<ul>
+	  <li><a class="active" href="index.php">Home</a></li>
+	  <li><a href="#news">News</a></li>
+	  <li><a href="#contact">Contact</a></li>
+	  <li><a href="#about">About</a></li>
+	</ul>
+</div>
 		<!-- // navbar -->
-        <?php include( ROOT_PATH . '/includes/banner.php') ?>
+        <div class="banner">
+	<div class="welcome_msg">
+		<h1>Today's Inspiration</h1>
+		<p> 
+		    One day your life <br> 
+		    will flash before your eyes. <br> 
+		    Make sure it's worth watching. <br>
+			<span>~ Gerard Way</span>
+		</p>
+		<a href="register.php" class="btn">Join us!</a>
+	</div>
+	<div class="login_div">
+		<form action="index.php" method="post" >
+			<h2>Login</h2>
+			<input type="text" name="username" placeholder="Username">
+			<input type="password" name="password"  placeholder="Password"> 
+			<button class="btn" type="submit" name="login_btn">Sign in</button>
+		</form>
+	</div>
+</div>
 		<!-- Page content -->
 		<div class="content">
 			<h2 class="content-title">Recent Articles</h2>
@@ -36,7 +72,12 @@
 		<!-- // Page content -->
 
 		<!-- footer -->
-		<?php include( ROOT_PATH . '/includes/footer.php') ?>
+		        <div class="footer">
+			<p>WTF Weekly &copy; <?php echo date('Y'); ?></p>
+        </div>
+	    <!-- // container -->
+    </body>
+</html>
 		<!-- // footer -->
 
 	
